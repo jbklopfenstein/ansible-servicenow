7caf3d330b2aa06523fe822cbde84daab56654a4
Jeff Klopfenstein <jklopfen@cisco.com>
2019-09-13 14:34:27 -0400

    removed files from codeon and added NOTICE

--- tools/check.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-# Add your linter specific validation  of your code. This includes running tests, running linter jobs
-# Heighliner executes checks.sh as a check job on Jenkins Server on every commit, before every build
--- tools/verify.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Include the verification steps here"
